23d87155bd96c209f0b591d019b4883ac9f809c3
austincrisp <austin.crisp05@gmail.com>
2017-01-10 00:22:50 -0500

    Hard Mode Completed; comments added

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -13,27 +13,74 @@ namespace PigLatin
             Console.WriteLine("Please enter a word: ");
             string word = Console.ReadLine();
 
-            char firstLetter = word[0];
-            char secondLetter = word[1];
-            string cleanFirst = firstLetter.ToString().ToUpper();
-            string cleanSecond = secondLetter.ToString().ToUpper();
-            string firstTwoLetters = cleanFirst + cleanSecond;
+            // Hard Mode trigger
+            if (word.Contains(" "))
+            {
+                Console.WriteLine("More than one word was entered.");                  // Since there is more than one word
+                Sentence();                                                            // Enter the Sentence function
+                return;
+            }
 
-            string[] vowel = new string[6] { "A", "E", "I", "O", "U", "Y" };
+            // Beginning of English to Pig Latin conversion logic
+            char firstLetter = word[0];                                                // look for the first letter in the user input string
+            char secondLetter = word[1];                                               // look for the second letter in the user input string
+            string cleanFirst = firstLetter.ToString().ToUpper();                      // change the first character into a string and let it work for lower and uppercase
+            string cleanSecond = secondLetter.ToString().ToUpper();                    // change the second character into a string and let it work for lower and uppercase
+            string firstTwoLetters = cleanFirst + cleanSecond;                         // Concatenate the first and second letters together
 
-            if (vowel.Contains(cleanFirst))
+            string[] vowel = new string[6] { "A", "E", "I", "O", "U", "Y" };           // storing out the vowels
+
+            if (vowel.Contains(cleanFirst))                                            // first check - first letter is a vowel
             {
                 Console.WriteLine($"{word + "way"}");
             }
-            else if (vowel.Contains(cleanSecond) && !vowel.Contains(cleanFirst))
+            else if (vowel.Contains(cleanSecond) && !vowel.Contains(cleanFirst))       // second check - first letter is not a vowel and second letter is a vowel
             {
-                string conVowel = word.Remove(0, 1);
+                string conVowel = word.Remove(0, 1);                                   // remove first letter of the word
                 Console.WriteLine($"{conVowel + cleanFirst + "ay"}");
             }
-            else if (!vowel.Contains(cleanFirst) && !vowel.Contains(cleanSecond))
+            else if (!vowel.Contains(cleanFirst) && !vowel.Contains(cleanSecond))      // third check - neither the first nor second letter is a vowel
             {
-                string doubleCon = word.Remove(0, 2);
+                string doubleCon = word.Remove(0, 2);                                  // remove first two letters of the word
                 Console.WriteLine($"{doubleCon + firstTwoLetters + "ay"}");
+            }
+            // Ending of English to Pig Latin conversion logic
+        }
+
+        // Hard Mode function
+        private static void Sentence()
+        {
+            Console.WriteLine("Please type a sentence: ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(' ');                                      // read in the space delimited sentence and store it into an array
+
+            foreach(string element in words)                                           // loop through each word and perform the conversion logic
+            {
+                // Beginning of English to Pig Latin conversion logic
+                char firstLetter = element[0];                                         // look for the first letter in element (word in the sentence)
+                char secondLetter = element[1];                                        // look for the second letter in element (word in the sentence)
+                string cleanFirst = firstLetter.ToString().ToUpper();                  // change the first character into a string and let it work for lower and uppercase
+                string cleanSecond = secondLetter.ToString().ToUpper();                // change the second character into a string and let it work for lower and uppercase
+                string firstTwoLetters = cleanFirst + cleanSecond;                     // Concatenate the first and second letters together
+
+                string[] vowel = new string[6] { "A", "E", "I", "O", "U", "Y" };       // storing out the vowels
+
+                if (vowel.Contains(cleanFirst))                                        // first check - first letter is a vowel
+                {
+                    Console.WriteLine($"{element + "way"}");
+                }
+                else if (vowel.Contains(cleanSecond) && !vowel.Contains(cleanFirst))   // second check - first letter is not a vowel and second letter is a vowel
+                {
+                    string conVowel = element.Remove(0, 1);                            // remove first letter of the word
+                    Console.WriteLine($"{conVowel + cleanFirst + "ay"}");
+                }
+                else if (!vowel.Contains(cleanFirst) && !vowel.Contains(cleanSecond))  // third check - neither the first nor second letter is a vowel
+                {
+                    string doubleCon = element.Remove(0, 2);                           // remove first two letters of the word
+                    Console.WriteLine($"{doubleCon + firstTwoLetters + "ay"}");
+                }
+                // Ending of English to Pig Latin conversion logic
             }
         }
     }
